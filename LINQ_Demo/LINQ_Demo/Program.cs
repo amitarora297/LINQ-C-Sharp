06e99096fc1bffe3623e8ac9ee4ebb399ef3f9f0
amitarora297 <40318164+amitarora297@users.noreply.github.com>
2018-07-11 23:49:40 +0530

    Initial Project Structure

---
+++ LINQ_Demo/LINQ_Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> lstEmployee = new List<Employee>();
+            Employee emp = new Employee() { Name = "Scott" };
+            lstEmployee.Add(emp);
+            emp = new Employee() { Name = "Allen" };
+            lstEmployee.Add(emp);
+            emp = new Employee() { Name = "David" };
+            lstEmployee.Add(emp);
+
+            var lstFilteredEmployee =  lstEmployee.Where(delegate(Employee argEmp)
+            {
+                return argEmp.Name.StartsWith("S");
+            });
+
+            foreach (var item in lstFilteredEmployee)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();  
+        }
+
+        private static bool startWithS(Employee emp)
+        {
+            return emp.Name.StartsWith("S");
+        }
+    }
+
+    class Employee
+    {
+        public string  Name { get; set; }
+    }
+
+}
